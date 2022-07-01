5b3b1a7a89bb6fa63335c2020453d8c493e5f14d
BeniDiegoVlog <benjamin.donofrio@outlook.fr>
2022-07-01 21:53:41 +0200

    ajouts de la page de contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import { FaInstagram } from 'react-icons/fa';
+import { HiOutlineMail, HiPhone } from 'react-icons/hi';
+import { ImFacebook2 } from 'react-icons/im';
+
 import SmartBar from '../components/SmartBar';
 import Menu from '../components/Menu';
-import Contacts from '../components/Contacts'
+// import Contacts from '../components/Contacts'
 import BottomBar from '../components/BottomBar';
 
 function Contact() {
@@ -11,10 +15,17 @@ function Contact() {
     <>
       <SmartBar />
       <Menu />
-      <div className="Droite">
-        <h3>Contact</h3>
+      <div className="Contact">
+        <div className="Contact-Gauche">
+          <h3><HiPhone /> Téléphone : <a className="Contacts" href="tel:+33770102320">+33770102320</a></h3>
+          <h3><HiOutlineMail /> Email : <a className="Contacts" href="mailto:maxime.turpault92@gmail.com?subject=Contact depuis votre Portfolio 💻 - Prénom Nom - Objet de votre mail&body=N'oubliez pas de mettre votre message ⭐">maxime.turpault92@gmail.com</a></h3>
+        </div>
+        <div className="Contact-Droite">
+          <h3><FaInstagram /> Instagram : <a className="Contacts" href='https://www.instagram.com/maximeturpault/' target="_blank" rel="noreferrer">/maximeturpault</a></h3>
+          <h3><ImFacebook2 /> Facebook : <a className="Contacts" href='https://www.facebook.com/profile.php?id=100012629315440' target="_blank" rel="noreferrer">Maxime Turpault</a></h3>
+        </div>
       </div>
-      <Contacts />
+      {/* <Contacts /> */}
       <BottomBar />
     </>
   );
