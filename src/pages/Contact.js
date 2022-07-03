b2289403e05a212e3bdda722b89f37036a9a4555
BeniDiegoVlog <benjamin.donofrio@outlook.fr>
2022-07-03 18:44:42 +0200

    creation du formulaire de contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -17,13 +17,44 @@ function Contact() {
       <Menu />
       <div className="Contact">
         <div className="Contact-Gauche">
-          <h3><HiPhone style={{marginRight: '5px'}} /> Téléphone : <a className="Contacts" href="tel:+33770102320">+33770102320</a></h3>
-          <h3><HiOutlineMail style={{marginRight: '5px'}} /> Email : <a className="Contacts" href="mailto:maxime.turpault92@gmail.com?subject=Contact depuis votre Portfolio 💻 - Prénom Nom - Objet de votre mail&body=N'oubliez pas de mettre votre message ⭐">maxime.turpault92@gmail.com</a></h3>
+          <h3><HiPhone style={{ marginRight: '5px' }} /> Téléphone : <a className="Contacts" href="tel:+33770102320">+33770102320</a></h3>
+          <h3><HiOutlineMail style={{ marginRight: '5px' }} /> Email : <a className="Contacts" href="mailto:maxime.turpault92@gmail.com?subject=Contact depuis votre Portfolio 💻 - Prénom Nom - Objet de votre mail&body=N'oubliez pas de mettre votre message ⭐">maxime.turpault92@gmail.com</a></h3>
         </div>
         <div className="Contact-Droite">
-          <h3><FaInstagram style={{marginRight: '5px'}} /> Instagram : <a className="Contacts" href='https://www.instagram.com/maximeturpault/' target="_blank" rel="noreferrer">/maximeturpault</a></h3>
-          <h3><ImFacebook2 style={{marginRight: '5px'}} /> Facebook : <a className="Contacts" href='https://www.facebook.com/profile.php?id=100012629315440' target="_blank" rel="noreferrer">Maxime Turpault</a></h3>
+          <h3><FaInstagram style={{ marginRight: '5px' }} /> Instagram : <a className="Contacts" href='https://www.instagram.com/maximeturpault/' target="_blank" rel="noreferrer">/maximeturpault</a></h3>
+          <h3><ImFacebook2 style={{ marginRight: '5px' }} /> Facebook : <a className="Contacts" href='https://www.facebook.com/profile.php?id=100012629315440' target="_blank" rel="noreferrer">Maxime Turpault</a></h3>
         </div>
+      </div>
+      <div className="Contact-Form">
+          <form>
+            <div className="Form-Group">
+              <label className="Form-Label" >Prénom :</label>
+              <input type="text" className="Form-Input" name="firstname" placeholder="Prénom" />
+            </div>
+            <div className="Form-Group">
+              <label className="Form-Label" >Nom :</label>
+              <input type="text" className="Form-Input" name="lastname" placeholder="Nom" />
+            </div>
+            <div className="Form-Group">
+              <label className="Form-Label" >Adresse mail :</label>
+              <input type="email" className="Form-Input" name="email" placeholder="Adresse Mail" />
+            </div>
+            <div className="Form-Group">
+              <label className="Form-Label" >Numéro de téléphone :</label>
+              <input type="phone" className="Form-Input" name="phone" placeholder="Numéro de téléphone" />
+            </div>
+            <div className="Form-Group">
+              <label className="Form-Label" >Objet :</label>
+              <input type="text" className="Form-Input" name="object" placeholder="Objet" />
+            </div>
+            <div className="Form-Group">
+              <label className="Form-Label" >Votre message :</label>
+              <textarea className="Form-Text" name="message" placeholder="Votre message de contact" />
+            </div>
+            <div className="Form-Button">
+              <button className="Button" type="submit">Envoyer</button>
+            </div>
+          </form>
       </div>
       {/* <Contacts /> */}
       <BottomBar />
